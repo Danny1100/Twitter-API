10aa5e1d3bfda42eef4de89e49cf59ee0e55ad5b
Danny1100 <dannycai1100@gmail.com>
2021-09-03 15:16:32 +1000

    Update Twitter_Credentials.py

--- Twitter API/Twitter_Credentials.py
+++ Twitter API/Twitter_Credentials.py
@@ -1,4 +1,5 @@
-consumer_key = "XRSzxCOAhwkVsbSQrZiGcX9DV"
-consumer_secret = "GBDzhFvefHSmLSe5eN7hrA5U0ILAJwDiNll60mpRAIlpyLVf5N"
-access_token = "1005426907309993984-2D9meGKKV2M6zKZ563rYmcG1u4wQiV"
-access_token_secret = "K3cJ0wzE1Y7aIqAS2DN5dN2xE2B4WXJkhkLb5o3AqSGtq"
+#these have been left blank for security purposes
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
